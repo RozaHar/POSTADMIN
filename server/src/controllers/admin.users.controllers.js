51daefe8f010a2a0120ab422082caaca20900ed5
RozaHar <rozaharutyuny.129@gmail.com>
2023-03-06 19:19:39 +0400

    added_admin_users_update_category

--- server/src/controllers/admin.users.controllers.js
+++ server/src/controllers/admin.users.controllers.js
@@ -1,7 +1,17 @@
-const { getAllUsersAdmin } = require("../models/admin.users.models");
+const {
+  getAllUsersAdmin,
+  updateUserCategory,
+} = require("../models/admin.users.models");
 
 async function getAllUsers(req, res) {
   return res.status(200).json(await getAllUsersAdmin());
 }
 
-module.exports = { getAllUsers };
+async function updateCategory(req, res) {
+  const id = req.params.id;
+  const user = req.body;
+  await updateUserCategory(id, user);
+  console.log(user, "category");
+  return res.status(200).json(user);
+}
+module.exports = { getAllUsers, updateCategory };
